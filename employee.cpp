d7ce9c7a5f92dcc636612ace9258084ed8477647
Dennisorf2020 <148282017+Dennisorf2020@users.noreply.github.com>
2023-11-04 02:06:25 -0500

    Create employee.cpp

---
+++ employee.cpp
@@ -0,0 +1,15 @@
+E#include "Employee.h";
+
+Employee::Employee(std::string theName, double theWage, double theHours) {
+    name = theName;
+    wage = theWage;
+    hours = theHours;
+}
+
+double Employee::calcPay() const {
+    return wage * hours;
+}
+
+std::string Employee::getName() const {
+    return name;
+}
